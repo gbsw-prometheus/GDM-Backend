042d38aae851d5fdd67ca7aa1701f470bfbb755c
joonho4 <wns4090@gmail.com>
2025-06-16 08:53:12 +0900

    fix:swagger 500 error

--- src/main/kotlin/com/apple/team_prometheus/global/exception/handler/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/apple/team_prometheus/global/exception/handler/GlobalExceptionHandler.kt
@@ -1,14 +1,25 @@
 package com.apple.team_prometheus.global.exception.handler
 
+import com.apple.team_prometheus.domain.attendance.controller.AttendanceController
+import com.apple.team_prometheus.domain.auth.controller.AuthController
+import com.apple.team_prometheus.domain.going.controller.GoingController
+import com.apple.team_prometheus.domain.meal.controller.MealController
+import com.apple.team_prometheus.domain.notification.controller.NotificationController
 import com.apple.team_prometheus.global.exception.Exceptions
 import com.apple.team_prometheus.global.exception.dto.ErrorMessage
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
-@RestControllerAdvice
-class GlobalExceptionHandler {
+
+@RestControllerAdvice(
+    annotations = [RestController::class],
+    basePackageClasses = [AuthController::class, MealController::class, NotificationController::class,
+        AttendanceController::class, GoingController::class]
+)
+class GlobalExceptionHandler{
 
     @ExceptionHandler(Exceptions::class)
     fun handleCustomExceptions(e: Exceptions): ResponseEntity<ErrorMessage.ErrorResponse> {
